35acf6aa7bbbca08e4ddf00fba5dfa777938b463
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-11-26 22:19:09 +0900

    function型

--- app.ts
+++ app.ts
@@ -1,10 +1,18 @@
-function add(n1: number, n2: number): number {
+function add(n1: number, n2: number) {
     return n1 + n2;
 }
 
 function printResult(num: number): void {
     console.log("Result: " + num);
 }
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+
+console.log(combineValues(8, 8));
 
 printResult(add(5, 12));
 // console.log(printResult(add(5, 12)));
