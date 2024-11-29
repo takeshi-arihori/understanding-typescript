f7f34520c4d8f385d600a1264da415d5bda651b2
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-11-29 21:59:55 +0900

    Never型

--- app.ts
+++ app.ts
@@ -4,4 +4,14 @@ let userName: string;
 userInput = 5;
 userInput = "Max";
 
-userName = userInput;
+if (typeof userInput === "string") {
+    userName = userInput;
+}
+
+function generateError(message: string, code: number): never {
+    throw { message: message, errorCode: code };
+}
+
+const result = generateError("エラーが発生しました", 500);
+
+console.log(result);
