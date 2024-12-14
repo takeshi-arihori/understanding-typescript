0840668e437776659693083ea617fe9887bcce19
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2024-12-14 09:17:08 +0900

    deguggerを使用

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,10 @@
 "use strict";
+let appId = "abc";
 const button = document.querySelector("button");
-function clickHandler(message) {
-    let userName = "Max";
+function clickHandler(message, age) {
     console.log("Clicked!! " + message);
 }
 if (button) {
-    button.addEventListener("click", clickHandler.bind(null, "You're welcome!"));
+    button.addEventListener("click", clickHandler.bind(null, "You're welcome!", 30));
 }
+//# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,13 @@ let appId = "abc";
 
 const button = document.querySelector("button")!;
 
-function add(n1: number, n2: number) {
-    if (n1 + n2 > 0) return n1 + n2;
-    // return;
-}
+// function add(n1: number, n2: number) {
+//     if (n1 + n2 > 0) return n1 + n2;
+//     // return;
+// }
 
 function clickHandler(message: string, age: number) {
-    let userName = "Max";
+    // let userName = "Max";
     console.log("Clicked!! " + message);
 }
 
